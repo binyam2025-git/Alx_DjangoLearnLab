9bd0d16b4d65beaa948abaa1554c259ae9e92c68
binyam <binyamwork@gmail.com>
2025-07-14 11:42:17 +0300

    f' the following is the way of commiting the followng task notepad urls.py

--- django-models/LibraryProject/LibraryProject/urls.py
+++ django-models/LibraryProject/LibraryProject/urls.py
@@ -1,3 +1,9 @@
+# LibraryProject/LibraryProject/urls.py
+# This is the MAIN project-level URL configuration
+
+from django.contrib import admin
+from django.urls import path, include # Make sure 'include' is here
+
 """
 URL configuration for LibraryProject project.
 
@@ -19,4 +25,7 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('relationship_app/', include('relationship_app.urls')), # <--- THIS IS THE CRUCIAL LINE for your app
+    # If you have a 'bookshelf' app and its urls.py, you would also include it here, e.g.:
+    # path('bookshelf/', include('bookshelf.urls')),
 ]
--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,14 +1,3 @@
-# LibraryProject/LibraryProject/urls.py
-
-from django.contrib import admin
-from django.urls import path, include # <--- Make sure 'include' is here
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('relationship_app/', include('relationship_app.urls')), # <--- ADD THIS LINE
-    # You might also have a path for 'bookshelf' here already, e.g.:
-    # path('bookshelf/', include('bookshelf.urls')),
-]
 from django.urls import path
 from . import views # Import views from the current app
 
