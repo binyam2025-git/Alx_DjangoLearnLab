d4fd9ac6aef36573e38232a697f0a58ef7377a68
binyam <binyamwork@gmail.com>
2025-07-14 14:32:14 +0300

    Confirmed and fixed main project urls.py; verified app views, urls, and templates are correct.

--- django-models/LibraryProject/LibraryProject/urls.py
+++ django-models/LibraryProject/LibraryProject/urls.py
@@ -2,29 +2,10 @@
 # This is the MAIN project-level URL configuration
 
 from django.contrib import admin
-from django.urls import path, include # Make sure 'include' is here
-
-"""
-URL configuration for LibraryProject project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path
+from django.urls import path, include # <--- Make sure 'include' is here
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls), # Standard Django admin URL
     path('relationship_app/', include('relationship_app.urls')), # <--- THIS IS THE CRUCIAL LINE for your app
     # If you have a 'bookshelf' app and its urls.py, you would also include it here, e.g.:
     # path('bookshelf/', include('bookshelf.urls')),
