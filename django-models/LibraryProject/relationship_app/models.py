8df3c9691211bb4b9f6bafc677d03e6430e6ae51
binyam <binyamwork@gmail.com>
2025-07-14 09:23:46 +0300

    f' the following is the task to compleat the comminting of the task, relationship_app/models.py

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
 # Create your models here.
+# Author Model
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+# Book Model (ForeignKey to Author)
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE) # ForeignKey relationship
+
+    def __str__(self):
+        return f"{self.title} by {self.author.name}"
+
+# Library Model (ManyToManyField to Book)
+class Library(models.Model):
+    name = models.CharField(max_length=200)
+    books = models.ManyToManyField(Book) # ManyToManyField relationship
+
+    def __str__(self):
+        return self.name
+
+# Librarian Model (OneToOneField to Library)
+class Librarian(models.Model):
+    name = models.CharField(max_length=100)
+    library = models.OneToOneField(Library, on_delete=models.CASCADE) # OneToOneField relationship
+
+    def __str__(self):
+        return f"{self.name} (Librarian at {self.library.name})"
