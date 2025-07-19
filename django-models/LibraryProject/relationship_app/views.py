4c0e0d1fa5612c9eae69b3f87c733b3007d3ab76
binyam <binyamwork@gmail.com>
2025-07-19 13:55:30 +0300

    f' the following is the way to commit the task of LibraryProject

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -6,7 +6,8 @@ from django.views.generic.detail import DetailView
 from .models import Author, Book, Library, Librarian
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login as auth_login
-from django.contrib.auth.decorators import login_required, user_passes_test, permission_required
+from django.contrib.auth.decorators import login_required, user_passes_test
+from django.contrib.auth.decorators import permission_required
 from django.urls import reverse_lazy
 
 # --- Function-Based View: List all Authors ---
