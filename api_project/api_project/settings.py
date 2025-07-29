104ed93b26f572a226b865439c89b8f80482cf55
binyam <binyamwork@gmail.com>
2025-07-29 10:08:13 +0300

    f' the following is a way to commit the file api_project

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(!sj2f!ic10*)&s@g6h8gk4%q2@huh!h2gie64kn(myn4b*)in'
+SECRET_KEY = 'django-insecure-umjt9k9&#6c@08h)(v!#8izb=e4!kyjrk579!2m@cn!73n&(dt'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
