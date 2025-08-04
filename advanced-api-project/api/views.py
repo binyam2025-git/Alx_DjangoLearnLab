c1e8f34bc26161c1548c1d4f4616d11033dfe3c7
binyam <binyamwork@gmail.com>
2025-08-04 12:06:42 +0300

    f' commiting the file advanced-api-project

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,6 +1,7 @@
 # api/views.py
 
 from rest_framework import generics, permissions, filters
+from django_filters import rest_framework
 from django_filters.rest_framework import DjangoFilterBackend
 from .models import Book
 from .serializers import BookSerializer
