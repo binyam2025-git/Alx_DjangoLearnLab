b8e7ba3c9e931ad0b4b5a56cba10a1f74281939f
binyam <binyamwork@gmail.com>
2025-08-17 11:21:24 +0300

    f' commiting a file dhango_blog

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -4,6 +4,7 @@ from django.conf import settings
 from django.utils import timezone
 from django.urls import reverse
 from taggit.managers import TaggableManager
+from django.contrib.auth.models import User
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
