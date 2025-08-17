abc8341c487cb613ef80ff88118f9327d363b1ad
binyam <binyamwork@gmail.com>
2025-08-17 10:43:06 +0300

    f' git commit django_blog

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -14,14 +14,16 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={'pk': self.pk})
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     content = models.TextField()
-    created_date = models.DateTimeField(default=timezone.now)
-    #created_at = models.DateTimeField(auto_now_add=True)
-    #updated_at = models.DateTimeField(auto_now=True)
+    #created_date = models.DateTimeField(default=timezone.now)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f'Comment by {self.author} on {self.post}'
