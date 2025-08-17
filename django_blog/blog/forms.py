2894b76300db11b916a5a7faa0eeccbf2e1da662
binyam <binyamwork@gmail.com>
2025-08-17 10:49:35 +0300

    f' commiting a file django_blog

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.contrib.auth import get_user_model
-from .models import Post
+from .models import Post, Comment
 
 User = get_user_model()
 
@@ -21,3 +21,8 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['title', 'content']
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['content']
