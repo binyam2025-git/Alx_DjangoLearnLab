3b349bc6df176cc6399f4335f0d89f52126f05c2
binyam <binyamwork@gmail.com>
2025-08-18 10:34:37 +0300

    f' coming a task, social_media_api

---
+++ social_media_api/accounts/models.py
@@ -0,0 +1,14 @@
+#from django.db import models
+
+# accounts/models.py
+
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class CustomUser(AbstractUser):
+    bio = models.TextField(blank=True, null=True)
+    profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True, null=True)
+    followers = models.ManyToManyField('self', related_name='following', symmetrical=False, blank=True)
+
+    def __str__(self):
+        return self.username
---
+++ social_media_api/accounts/views.py
@@ -0,0 +1,36 @@
+#from django.shortcuts import render
+
+# accounts/views.py
+
+from rest_framework import generics, status
+from rest_framework.response import Response
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
+from rest_framework.permissions import IsAuthenticated
+from .serializers import CustomUserSerializer
+from .models import CustomUser
+
+class RegisterView(generics.CreateAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = CustomUserSerializer
+
+class CustomAuthToken(ObtainAuthToken):
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data,
+                                           context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({
+            'token': token.key,
+            'user_id': user.pk,
+            'email': user.email
+        })
+
+class ProfileView(generics.RetrieveUpdateAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = CustomUserSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
