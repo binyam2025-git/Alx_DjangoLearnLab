1585cb2205a6f53e2ef0a4146ee41f050f13fbdb
binyam <binyamwork@gmail.com>
2025-08-20 13:59:06 +0300

    f' commiting the task social_media_api

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -5,9 +5,16 @@ from .models import Post, Like, Comment  # Ensure Comment is imported
 from .serializers import PostSerializer, CommentSerializer  # Ensure CommentSerializer is imported
 
 class PostViewSet(viewsets.ModelViewSet):
-    queryset = Post.objects.all()
     serializer_class = PostSerializer
     permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        # Get the current user
+        user = self.request.user
+        # Get the list of users the current user is following
+        following_users = user.following.all()  # Adjust this if following is a ManyToMany field
+        # Filter posts by those users
+        return Post.objects.filter(author__in=following_users).order_by('-created_at')
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
