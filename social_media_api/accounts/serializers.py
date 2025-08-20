7cf470742107491df88068eb68309326f1a8af75
binyam <binyamwork@gmail.com>
2025-08-20 12:08:10 +0300

    f' commmit the task social media api

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,18 +1,17 @@
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
-from .models import CustomUser
 
 class CustomUserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
 
     class Meta:
-        model = CustomUser
+        model = get_user_model()  # Use the custom user model
         fields = ['id', 'username', 'email', 'password']  # Add other fields as needed
 
     def create(self, validated_data):
-        user = CustomUser(
+        user = get_user_model().objects.create_user(
             username=validated_data['username'],
-            email=validated_data['email']
+            email=validated_data['email'],
+            password=validated_data['password']
         )
-        user.set_password(validated_data['password'])  # Safely set the password
-        user.save()
         return user
