455fd670a52ef388e4e7470171f421109880cb97
binyam <binyamwork@gmail.com>
2025-08-20 12:15:50 +0300

    f' committing the task social media api

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -3,7 +3,9 @@ from rest_framework import serializers
 from rest_framework.authtoken.models import Token  # Import Token
 
 class CustomUserSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
+    username = serializers.CharField(required=True)  # Ensure username is a CharField
+    email = serializers.EmailField(required=True)     # Email field
+    password = serializers.CharField(write_only=True)  # Password field
 
     class Meta:
         model = get_user_model()  # Use the custom user model
