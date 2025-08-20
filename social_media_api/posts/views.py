6d9d3dbc43b67c2633a7d3fb894071990fb1c4e2
binyam <binyamwork@gmail.com>
2025-08-20 12:01:25 +0300

    f' committing the file social media api

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,14 +1,10 @@
-from rest_framework import generics, status, permissions
+from rest_framework import viewsets, permissions
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
-from .models import Post, Like
-from notifications.models import Notification
-from django.contrib.contenttypes.models import ContentType
-from .serializers import PostSerializer
-from django.db.models import Q
-from accounts.models import CustomUser
+from .models import Post, Like, Comment  # Ensure Comment is imported
+from .serializers import PostSerializer, CommentSerializer  # Ensure CommentSerializer is imported
 
-class PostListCreateView(generics.ListCreateAPIView):
+class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
     permission_classes = [permissions.IsAuthenticated]
@@ -16,51 +12,13 @@ class PostListCreateView(generics.ListCreateAPIView):
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
 
-class PostDetailView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
     permission_classes = [permissions.IsAuthenticated]
 
-    def get_object(self):
-        return get_object_or_404(Post, pk=self.kwargs['pk'])
+    def perform_create(self, serializer):
+        post = get_object_or_404(Post, pk=self.request.data['post'])
+        serializer.save(author=self.request.user, post=post)
 
-class FeedView(generics.ListAPIView):
-    serializer_class = PostSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-    def get_queryset(self):
-        #following = self.request.user.following.all()
-        following_users = self.request.user.following.all()
-        return Post.objects.filter(author__in=following_users).order_by('-created_at')
-
-class LikePostView(generics.GenericAPIView):
-    permission_classes = [permissions.IsAuthenticated]
-
-    def post(self, request, pk):
-        post = generics.get_object_or_404(Post, pk=pk)
-        like, created = Like.objects.get_or_create(user=request.user, post=post)
-
-        if created:
-            # Create a notification for the post author
-            Notification.objects.create(
-                recipient=post.author,
-                actor=request.user,
-                verb='liked your post',
-                target_ct=ContentType.objects.get_for_model(Post),
-                target_id=post.id
-            )
-            return Response({"message": "Post liked!"}, status=201)
-        
-        return Response({"message": "You already liked this post!"}, status=400)
-
-class UnlikePostView(generics.GenericAPIView):
-    permission_classes = [permissions.IsAuthenticated]
-
-    def post(self, request, pk):
-        post = get_object_or_404(Post, pk=pk)
-        
-        # Get the Like object or return a 404 if not found
-        like = get_object_or_404(Like, user=request.user, post=post)
-        like.delete()
-        
-        return Response({"message": "Post unliked!"}, status=204)
+# Other views (like, unlike, etc.) can remain as they are or be adapted as needed.
