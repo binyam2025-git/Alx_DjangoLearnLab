2abcc672e756b9784a7e99286b1ea68969dcb430
binyam <binyamwork@gmail.com>
2025-08-20 11:34:32 +0300

    f' commiting the task social media api

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -37,7 +37,7 @@ class LikePostView(generics.GenericAPIView):
     permission_classes = [permissions.IsAuthenticated]
 
     def post(self, request, pk):
-        post = get_object_or_404(Post, pk=pk)
+        post = generics.get_object_or_404(Post, pk=pk)
         like, created = Like.objects.get_or_create(user=request.user, post=post)
 
         if created:
